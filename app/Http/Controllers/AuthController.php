f365b4e084376c62dce9d2c0a46400b18dfd6b19
axlkun <axelcruz.dev@gmail.com>
2024-02-23 15:15:23 -0600

    add register method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+
+    public function register(request $request){
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make( $request->input('password')),
+        ]);
+
+        return $user;
+    }
+
+    public function user(){
+        return 'Authenticated user';
+    }
 }
