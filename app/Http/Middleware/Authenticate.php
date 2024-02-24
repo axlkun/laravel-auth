f6d67eb1d438c2b09d39679257d89e029c512775
axlkun <axelcruz.dev@gmail.com>
2024-02-24 00:09:06 -0600

    configure middleware to add jwt to headers

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -3,24 +3,27 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Http\Request;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return string|null
      */
-    protected function redirectTo(Request $request): ?string
+    protected function redirectTo($request)
     {
-        return $request->expectsJson() ? null : route('login');
+        if (!$request->expectsJson()) {
+            return route('login');
+        }
     }
 
     public function handle($request, Closure $next, ...$guards)
     {
-
-        if($jwt = $request->cookie('jwt')){
-            $request->headers->set('Authorization','Bearer ',$jwt);
+        if ($jwt = $request->cookie('jwt')) {
+            $request->headers->set('Authorization', 'Bearer ' . $jwt);
         }
 
         $this->authenticate($request, $guards);
