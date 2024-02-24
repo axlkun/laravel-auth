5cadce6e223b0465dca729ba861c33de6f521d9d
axlkun <axelcruz.dev@gmail.com>
2024-02-23 19:22:13 -0600

    protect the route

--- routes/api.php
+++ routes/api.php
@@ -15,6 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('user',[AuthController::class,'user']);
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
+
+Route::middleware('auth:sanctum')->group(function(){
+    Route::get('user',[AuthController::class,'user']);
+});
+
